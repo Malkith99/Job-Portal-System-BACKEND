93bc0ea51ee9a41b0f5f43ddc9893bb5c05fa0d6
Malkith99 <malkithamanda@gmail.com>
2023-01-10 23:27:49 +0530

    Database is connected

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -4,7 +4,24 @@ const bodyParser=require("body-parser");
 const cors = require("cors");
 const dotenv=require("dotenv");
 const app =express();
+require("dotenv").config();
 
 const PORT =process.env.PORT || 8070;
-app.use(cors());
-app.use(bodyParser.json())
+app.use(cors());                        // use the cors package
+app.use(bodyParser.json())              // in mongodb there exist json format(key value pairs)
+const URL=process.env.MONGODB_URL;
+
+mongoose.connect(URL,{
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopologyL: true,
+    useFindAndModify: false
+}); 
+
+ const connection = mongoose.connection;
+ connection.once("open",()=>{
+    console.log("Mongodb connection success!");
+ })
+ app.listen(PORT,()=> {
+    console.log(`Server is up and running on port number: |${PORT}`)
+ })
