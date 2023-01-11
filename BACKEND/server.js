10329493c9945d05109c72bcb3bfe5eade5eda9d
Malkith99 <malkithamanda@gmail.com>
2023-01-11 11:27:52 +0530

    Linked with DataBase

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -4,24 +4,30 @@ const bodyParser=require("body-parser");
 const cors = require("cors");
 const dotenv=require("dotenv");
 const app =express();
-require("dotenv").config();
+require("dotenv").config();               // use the dotenv file
 
 const PORT =process.env.PORT || 8070;
 app.use(cors());                        // use the cors package
 app.use(bodyParser.json())              // in mongodb there exist json format(key value pairs)
 const URL=process.env.MONGODB_URL;
+  
+mongoose.set('strictQuery',true);
 
-mongoose.connect(URL,{
+/*mongoose.connect(URL,{
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopologyL: true,
     useFindAndModify: false
-}); 
+}); */
 
  const connection = mongoose.connection;
  connection.once("open",()=>{
     console.log("Mongodb connection success!");
  })
  app.listen(PORT,()=> {
-    console.log(`Server is up and running on port number: |${PORT}`)
+    console.log(`Server is up and running on port number: ${PORT}`)
  })
+
+ app.get("/", res, req){
+   req
+ }
