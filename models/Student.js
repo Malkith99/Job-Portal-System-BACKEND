d987d80f092deaeaeaa6ca6e553aff0ad0f96297
Malkith99 <malkithamanda@gmail.com>
2023-04-01 22:42:04 +0530

    change the Student model for Sign Up process

--- models/Student.js
+++ models/Student.js
@@ -3,18 +3,33 @@ const mongoose=require('mongoose');       // assign mongoose package to const va
 const Schema=mongoose.Schema;           //Schema- template of a document mongodb
 
 const studentSchema=new Schema({
-    name:{
+   /* fname:{
         type: String,
         required :true              //  required-make it required 
     },
-    age:{
+    lname:{
+        type: String,
+        required :true              //  required-make it required 
+    },
+        age:{
         type:Number,                // Number-primitive data type in js
         required:true
     },
     gender:{
         type:String,
         required:true
-    }
+    }*/
+    email:{
+        type: String,
+        required :true,    
+        unique :true          //  email address should be unique (only one should exist)
+    },
+    password:{
+        type: String,
+        required :true              //  required-make it required 
+    },
+
+
 })
 
 const Student=mongoose.model("Student",studentSchema);   //"Student"-Name of the schema 
