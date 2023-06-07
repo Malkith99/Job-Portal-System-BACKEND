8d2ee9c18ffe9a1bbeb7d09f764ee5a96b6ce0cc
Supun Thilakshana Dharmarathna <82252921+SupunTd@users.noreply.github.com>
2023-06-07 08:17:59 +0530

    update server.js

--- server.js
+++ server.js
@@ -1,33 +1,58 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+const express=require("express");          // declare variable and assign the package to it
+const mongoose= require("mongoose");       // we can use these functions to use functionalities of each packages
+const bodyParser=require("body-parser");
 const cors = require("cors");
-require("dotenv").config();
+const dotenv=require("dotenv");
+const app =express();
+require('dotenv').config();               // use the dotenv file which has mongodb url
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-const MONGODB_URL = process.env.MONGODB_URL;
 
-app.use(cors());
-app.use(bodyParser.json());
+const PORT =process.env.PORT || 1234;   // logical or operation
+app.use(cors());                        // use the cors package
+app.use(bodyParser.json())              // in mongodb there exist json format(key value pairs)
 
-mongoose.connect(MONGODB_URL, {
+const URL=process.env.MONGODB_URL;
+
+mongoose.set('strictQuery',true);
+
+
+/*mongoose.connect(URL,{
+    useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-    .then(() => {
-        console.log("MongoDB connection success!");
-        app.listen(PORT, () => {
-            console.log(`Server is up and running on port number: ${PORT}`);
-        });
-    })
-    .catch((error) => {
-        console.error("MongoDB connection failed:", error);
-    });
+    useUnifiedTopologyL: true,
+    useFindAndModify: false
+    }); */
 
-// Define and use your routes here
-const studentRouter = require("./routes/students.js");
-app.use("/student", studentRouter);
+/*const connection = mongoose.connection;
+connection.once("open",()=>{
+console.log("Mongodb connection success!");
+  })*/
 
-const mycompanyRouter = require("./routes/companies.js");
-app.use("/company", mycompanyRouter);
+const database=(module.exports=()=>{
+    const connectionParams={
+        useNewUrlParser: true,
+        useUnifieldTopology: true,
+
+    };
+    try{
+        mongoose.connect(
+            "mongodb+srv://malkithamanda:99Mathematics@cluster0.qvv4jwk.mongodb.net/jobBank_db?retryWrites=true&w=majority");
+        console.log("Mongodb connection success!");
+    } catch(err){
+        console.log(err);
+        console.log("Database connection failed");
+    }
+});
+
+app.listen(PORT,()=> {
+    console.log(`Server is up and running on port number: ${PORT}`);
+});
+
+database();
+
+const studentRouter= require("./routes/students.js");      // import students.js to studentRouter
+app.use("/student",studentRouter);              // When calling /student URL , it loads file the studentRouter that assign to studentRouter varibale
+
+
+const mycompanyRouter= require("./routes/companies.js");      // import students.js to studentRouter
+app.use("/company",mycompanyRouter);              // When calling /student URL , it loads file the studentRouter that assign to studentRouter variable
