44b44f86401e6442e89706e0f0458440ee9940e1
Malkith99 <malkithamanda@gmail.com>
2023-07-08 10:05:05 +0530

    syntaxError is fixed in auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,7 @@ const Joi = require("joi");
 
 router.post("/", async (req, res) => {
     try {
-        const { error } = validate(req.body);
+        const { error } = validateData(req.body);
         if (error)
             return res.status(400).send({ message: error.details[0].message });
 
@@ -21,7 +21,6 @@ router.post("/", async (req, res) => {
             return res.status(401).send({ message: "Invalid Email or Password" });
 
         if(!user.verified){
-
             return res.status(400).send({message: "User Not Verified"})
         }
 
@@ -29,16 +28,12 @@ router.post("/", async (req, res) => {
         console.log(`User ${user._id} has been login via authentication `);
         console.log(`User Token created `);
         res.status(200).send({ message: `logged in successfully ${user.firstName}`, token: token, user: user});
-
-
-
-
     } catch (error) {
         res.status(500).send({ message: "Internal Server Error login" });
     }
 });
 
-const validate = (data) => {
+const validateData = (data) => {
     const schema = Joi.object({
         email: Joi.string().email().required().label("Email"),
         password: Joi.string().required().label("Password"),
@@ -46,8 +41,4 @@ const validate = (data) => {
     return schema.validate(data);
 };
 
-
-
-
 module.exports = router;
-
